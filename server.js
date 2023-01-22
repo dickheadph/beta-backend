f63b30e67ad1b4bd639fe1c97bd7ab7d9c5f22e5
shandie <shancarl.belgica.3@gmail.com>
2023-01-23 00:03:59 +0800

    added fixes

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const env = require('dotenv');
 
 env.config({ path: './.env' });
 //const port = process.env.PORT || 8080;
-const db = process.env.MONGO_URI;
+//const db = process.env.MONGO_URI;
 //console.log(db);
-//mongoose.set('strictQuery', true);
-mongoose
-  .connect(db)
+mongoose.set('strictQuery', true);
+await mongoose
+  .connect(
+    'mongodb+srv://shana:typhoon150@cluster0.w4we942.mongodb.net/Portfolio?retryWrites=true&w=majority'
+  )
   .then(() => {
     console.log('Connected to MongoDB_ATLAS');
   })
