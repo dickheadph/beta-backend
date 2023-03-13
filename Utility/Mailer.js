2405714cd19bca324253d9ef2dcb34c3d6cc931b
shandie <shancarl.belgica.3@gmail.com>
2023-03-13 16:55:08 +0800

    MAIL TEST

--- Utility/Mailer.js
+++ Utility/Mailer.js
@@ -4,13 +4,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   //create transport
   const transpoter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: 587,
-    // auth: {
-    //   user: process.env.EMAIL_USER,
-    //   pass: process.env.EMAIL_PASS,
-    // },
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: 587,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
